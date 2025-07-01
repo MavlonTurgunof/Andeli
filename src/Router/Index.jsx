c0a9b8fb57638c15717b5276ddd6d5e78ed2b642
MavlonTurgunof <mavlondev2023@gmail.com>
2025-07-01 13:36:42 +0500

    udpate

--- src/Router/Index.jsx
+++ src/Router/Index.jsx
@@ -7,20 +7,23 @@ import Contact from "../../pages/Contact";
 import AppLayout from "../Components/AppLayout";
 import AllProducts from "../../pages/AllProducts";
 import ProductDetailPage from "../../pages/ProductDetailPage";
+import ScrollToTop from "../Components/ScrollToTop";
 
 function Index() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AppLayout />}>
-          <Route index element={<Home />} />
-          <Route path="products" element={<Products />} />
-          <Route path="/products/detail" element={<ProductDetailPage />} />
-          <Route path="products/allproducts" element={<AllProducts />} />
-          <Route path="ai-assistant" element={<AIasistent />} />
-          <Route path="contact" element={<Contact />} />
-        </Route>
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Home />} />
+            <Route path="products" element={<Products />} />
+            <Route path="/products/detail" element={<ProductDetailPage />} />
+            <Route path="products/allproducts" element={<AllProducts />} />
+            <Route path="ai-assistant" element={<AIasistent />} />
+            <Route path="contact" element={<Contact />} />
+          </Route>
+        </Routes>
+      </ScrollToTop>
     </BrowserRouter>
   );
 }
